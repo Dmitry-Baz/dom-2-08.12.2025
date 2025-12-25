123da942191913d52d5071fa1cdda8abd25ecb39
Dmitry-Baz <151816542+Dmitry-Baz@users.noreply.github.com>
2025-12-25 13:50:40 +0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,5 @@ import PlayingArea from './playing-area/playing-area';
 document.addEventListener('DOMContentLoaded', () => {
   const playingArea = new PlayingArea(document.querySelector('.playing-area'));
 
-  setInterval(playingArea.randomMovingGoblin, 1000);
+  playingArea.start();
 })
